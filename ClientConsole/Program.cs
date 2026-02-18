da8df74bcd6c3e979d0e80e7c59d27f7cbe4d466
Ahmet Koc <ahmetkoc.iletisim@gmail.com>
2026-02-18 15:35:23 +0300

    thread yerine task modeline geçildi

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -1,32 +1,33 @@
 ﻿using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
 string serverIP = "127.0.0.1";
 int port = 13000;
 
-TcpClient client = new TcpClient(serverIP, port);
+TcpClient client = new TcpClient();
+await client.ConnectAsync(serverIP, port);
+
 Console.WriteLine("Servera bağlanıldı.");
 
 NetworkStream stream = client.GetStream();
 
-Thread receiveThread = new Thread(() => ReceiveMessages(stream));
-receiveThread.Start();
+// Thread yerine Task 
+_ = ReceiveMessagesAsync(stream);
 
 while (true)
 {
     string message = Console.ReadLine();
+
     byte[] data = Encoding.UTF8.GetBytes(message);
-    stream.Write(data, 0, data.Length);
+    await stream.WriteAsync(data, 0, data.Length);
 }
 
-static void ReceiveMessages(NetworkStream stream)
+static async Task ReceiveMessagesAsync(NetworkStream stream)
 {
-    //byte olarak geliyo okumak için bufferliyoruz
     byte[] buffer = new byte[1024];
     int bytesRead;
 
-    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
     {
         string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
         Console.WriteLine("Server: " + data);
--- ServerConsole/Program.cs
+++ ServerConsole/Program.cs
@@ -1,44 +1,35 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 Int32 port = 13000;
 IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 TcpListener server = new TcpListener(localAddr, port);
 
 server.Start();
-Console.WriteLine("Server Başlatıldı ");
-using TcpClient client = server.AcceptTcpClient();
+Console.WriteLine("Server Başlatıldı");
+
+using TcpClient client = await server.AcceptTcpClientAsync();
 NetworkStream stream = client.GetStream();
 
-Thread receiveThread = new Thread(() => ReceiveMessages(stream));
-receiveThread.Start();
-
-//thread sınıfı metoda param gönderebilmek için tek tip kabul ediyor o yüzden object alıyoruz
-//biz de metodun içinde (TcpClient)obj yazarak, aslında gönderdiğimiz nesnenin gerçek tipinin TcpClient olduğunu söyleyip tekrar kendi tipine çeviriyoruz
-//yada lambda ifadesi kullanarak direkt olarak TcpClient nesnesini kullanabiliriz
+// Thread yerine Task
+_ = ReceiveMessagesAsync(stream);
 
 while (true)
 {
     string message = Console.ReadLine();
     byte[] data = Encoding.UTF8.GetBytes(message);
-    stream.Write(data, 0, data.Length);
+    await stream.WriteAsync(data, 0, data.Length);
 }
-static void ReceiveMessages(NetworkStream stream)
+
+static async Task ReceiveMessagesAsync(NetworkStream stream)
 {
-    
-    int bytesRead;
     byte[] buffer = new byte[1024];
+    int bytesRead;
 
-    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
     {
-        string data;
-        data = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
         Console.WriteLine("Gelen mesaj " + data);
-
-     
     }
 }
