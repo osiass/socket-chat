b9a984464180072537f15cc9ae0fecde74e36b4d
Ahmet Koc <ahmetkoc.iletisim@gmail.com>
2026-02-19 15:45:23 +0300

    mssql ile login-register sistemi eklendi

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -11,6 +11,50 @@ Console.WriteLine("Servera bağlanıldı.");
 
 NetworkStream stream = client.GetStream();
 
+Console.WriteLine("1-Giriş Yap");
+Console.WriteLine("2-Kayıt Ol");
+
+string secim = Console.ReadLine();
+
+Console.Write("Kullanıcı adınızı girin: ");
+string username = Console.ReadLine();
+
+Console.Write("Şifrenizi girin: ");
+string password = Console.ReadLine();
+
+if (secim == "1")
+{
+    string loginMessage = $"LOGIN|{username}|{password}";
+    byte[] data = Encoding.UTF8.GetBytes(loginMessage);
+
+    await stream.WriteAsync(data, 0, data.Length);
+}
+else if (secim == "2")
+{
+    string registerMessage = $"REGISTER|{username}|{password}";
+    byte[] data = Encoding.UTF8.GetBytes(registerMessage);
+
+    await stream.WriteAsync(data, 0, data.Length);
+}
+else
+{
+    Console.WriteLine("Geçersiz seçim. Program sonlandırılıyor.");
+    return;
+}
+
+//buraya bi buffer ekleyip serverdan gelen cevaba göre giriş başarılı mı değil mi 
+byte[] buffer = new byte[1024];
+int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine("Server: " + response);
+
+if (!response.StartsWith("OK"))
+{
+    Console.WriteLine("Giriş başarısız.");
+    return;
+}
+
 // Thread yerine Task 
 _ = ReceiveMessagesAsync(stream);
 
@@ -33,3 +77,4 @@ static async Task ReceiveMessagesAsync(NetworkStream stream)
         Console.WriteLine("Server: " + data);
     }
 }
+
--- ServerConsole/Program.cs
+++ ServerConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Microsoft.Data.SqlClient;
 
 Int32 port = 13000;
 IPAddress localAddr = IPAddress.Parse("127.0.0.1");
@@ -22,7 +23,7 @@ while (true)
     await stream.WriteAsync(data, 0, data.Length);
 }
 
-static async Task ReceiveMessagesAsync(NetworkStream stream)
+async Task ReceiveMessagesAsync(NetworkStream stream)
 {
     byte[] buffer = new byte[1024];
     int bytesRead;
@@ -31,5 +32,58 @@ static async Task ReceiveMessagesAsync(NetworkStream stream)
     {
         string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
         Console.WriteLine("Gelen mesaj " + data);
+        
+        string[] parts = data.Split('|'); //login|ahmetk|1234 şeklinde geliyo
+
+
+        if (parts[0]== "REGISTER")
+        {
+            bool result = await RegisterUser(parts[1], parts[2]);
+
+            string response = result ? "OK REGISTER" : "ERROR REGISTER";
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+            await stream.WriteAsync(responseData, 0, responseData.Length);
+
+        }
+        else if(parts[0] == "LOGIN")
+        {
+            bool result = await LoginUser(parts[1], parts[2]);
+            string response = result ? "OK LOGIN" : "ERROR LOGIN";
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+            await stream.WriteAsync(responseData, 0, responseData.Length);
+        }
+        
     }
+}
+
+async Task<bool> RegisterUser(string username, string password)
+{
+
+    string dbConnectionString = "Server=localhost;Database=TcpChatDb;Trusted_Connection=True;TrustServerCertificate=True;";
+
+    using SqlConnection connection = new SqlConnection(dbConnectionString);
+    await connection.OpenAsync();
+
+    //buraya aynı kullanıcı adı var mı kontrolü eklenebilir
+
+    string insertQuery = "INSERT INTO dbo.Users (Username, Password) VALUES (@Username, @Password)";
+    using SqlCommand command = new SqlCommand(insertQuery, connection);
+    command.Parameters.AddWithValue("@Username", username);
+    command.Parameters.AddWithValue("@Password", password);
+
+    await command.ExecuteNonQueryAsync();
+    return true;
+}
+
+async Task<bool> LoginUser(string username, string password)
+{
+    string dbConnectionString = "Server=localhost;Database=TcpChatDb;Trusted_Connection=True;TrustServerCertificate=True;";
+    using SqlConnection connection = new SqlConnection(dbConnectionString);
+    await connection.OpenAsync();
+    string selectQuery = "SELECT COUNT(*) FROM dbo.Users WHERE Username = @Username AND Password = @Password";
+    using SqlCommand command = new SqlCommand(selectQuery, connection);
+    command.Parameters.AddWithValue("@Username", username);
+    command.Parameters.AddWithValue("@Password", password);
+    int userCount = (int)await command.ExecuteScalarAsync();
+    return userCount > 0;
 }
